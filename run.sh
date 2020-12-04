ac93a54d522cdd07dfc57a4958676bcafb9f2773
wimbiyoas <wyoashizkia@gmail.com>
2020-12-04 00:48:08 -0800

    Update

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 git clone https://$GITHUB_TOKEN@github.com/wimbiyoas/kernel_asus_sdm660
 cd kernel_asus_sdm660
-curl https://raw.githubusercontent.com/STRIX-Project/kernel_build_script/droneci/droneci-release-X00T.sh | bash
+curl https://raw.githubusercontent.com/wimbiyoas/myscripts/master/MASTER.sh | bash
