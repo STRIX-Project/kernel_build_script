ae0808cc84c577bd84462ff67c0f4ed159dbe371
wimbiyoas <wyoashizkia@gmail.com>
2020-12-03 23:54:19 -0800

    X00T: Change Repo

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-git clone https://$GITHUB_TOKEN@github.com/STRIX-Project/STRIX-X00T
-cd STRIX-X00T
+git clone https://$GITHUB_TOKEN@github.com/wimbiyoas/kernel_asus_sdm660
+cd kernel_asus_sdm660
 curl https://raw.githubusercontent.com/STRIX-Project/kernel_build_script/droneci/droneci-release-X00T.sh | bash
