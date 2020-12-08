ce2d72c68439a4b9bc5b3c89a4f85a02aa7be559
wimbiyoas <wyoashizkia@gmail.com>
2020-12-07 21:18:53 -0800

    heu

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 git clone https://$GITHUB_TOKEN@github.com/STRIX-Project/STRIX-X00T
 cd STRIX-X00T
-curl https://raw.githubusercontent.com/STRIX-PROJECT/kernel_build_script/droneci-release-X00T.sh | bash
+curl https://raw.githubusercontent.com/STRIX-PROJECT/kernel_build_script/droncei/droneci-release-X00T.sh | bash
