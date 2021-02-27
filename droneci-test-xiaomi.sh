af0ce18c4054f8b53392f2f2732bb11db02279df
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-02-27 15:43:17 +0700

    droneci-test-xiaomi: Change to personal GCC toolchains
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- droneci-test-xiaomi.sh
+++ droneci-test-xiaomi.sh
@@ -166,8 +166,8 @@ DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%T")
 			GCC32_DIR=$KERNEL_DIR/gcc32
 		else
 			msg "|| Cloning GCC 10.2.0 baremetal ||"
-			git clone https://github.com/theradcolor/aarch64-linux-gnu.git -b stable-gcc --depth=1 gcc64
-			git clone https://github.com/theradcolor/arm-linux-gnueabi.git -b stable-gcc --depth=1 gcc32
+			git clone https://github.com/fiqri19102002/aarch64-gcc.git -b gnu-gcc-10 --depth=1 gcc64
+			git clone https://github.com/fiqri19102002/arm-gcc.git -b gnu-gcc-10 --depth=1 gcc32
 			GCC64_DIR=$KERNEL_DIR/gcc64
 			GCC32_DIR=$KERNEL_DIR/gcc32
 		fi
