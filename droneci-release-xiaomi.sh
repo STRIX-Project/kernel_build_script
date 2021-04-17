68da24fdc3d3eb1fd1fc97087e2fb8139c780136
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-04-17 20:57:23 +0700

    droneci-release-xiaomi: Update zip name for EAS build 2021/04/17
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- droneci-release-xiaomi.sh
+++ droneci-release-xiaomi.sh
@@ -67,8 +67,8 @@ KERNELTYPE1=HMP
 KERNELRELEASE=stable
 
 # List the kernel version of each device
-VERSION=v4.2 # Tulip device
-VERSION1=v4.6 # Whyred device
+VERSION=v4.3 # Tulip device
+VERSION1=v4.7 # Whyred device
 VERSION2=v1.0 # Jasmine/wayne device
 VERSION3=v1.1 # for HMP branch (tulip & whyred)
 
